dfc664e1bc3e7be75dbc787c45e3d991b991df4b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-03-31 10:49:49 +0000

    Deploy vvkk77/requester-dashboard to github.com/vvkk77/requester-dashboard.git:gh-pages

---
+++ precache-manifest.1574477f9e68e2c12e68b14d94745e1e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5edde784406907bf937",
+    "url": "css/app.1c61be21.css"
+  },
+  {
+    "revision": "9d0238b4e57b97012dde90f61adf150a",
+    "url": "img/approvalwaiting-icon.9d0238b4.png"
+  },
+  {
+    "revision": "423d5a42de0d7207f7d70a052f86593c",
+    "url": "img/login-bg.423d5a42.png"
+  },
+  {
+    "revision": "de305c552c19922c1a5aeaa49d0bb9e5",
+    "url": "img/no-request.de305c55.png"
+  },
+  {
+    "revision": "186cee1fda745ce6467ca968af9cba0b",
+    "url": "index.html"
+  },
+  {
+    "revision": "e5edde784406907bf937",
+    "url": "js/app.065d70d5.js"
+  },
+  {
+    "revision": "5ff7d0b8ea2c8b84df9b",
+    "url": "js/chunk-vendors.ce2301ca.js"
+  },
+  {
+    "revision": "ae69ceac071030e56858757e5aff6d5e",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
